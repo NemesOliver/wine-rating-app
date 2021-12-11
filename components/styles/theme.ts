5c2a6f02c2964d137579cb59f7f246d56f803d1a
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-12-11 17:56:39 +0000

    create theme

---
+++ components/styles/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#780000",
+      main: "#AC0000",
+      dark: "#bc3333",
+    },
+    secondary: {
+      main: "#ff6e40",
+    },
+  },
+});
