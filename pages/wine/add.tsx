69fdf93702dea8a50843848b71eafa880871945b
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-12-16 15:12:08 +0000

    add form validation

--- pages/wine/add.tsx
+++ pages/wine/add.tsx
@@ -2,6 +2,7 @@ import { useState, useContext } from "react";
 import { useRouter } from "next/router";
 import initializeFirebase from "../../firebase";
 import { SnackbarContext } from "../../context/SnackbarContext";
+import { AuthContext } from "../../context/AuthContext";
 import { getFirestore, addDoc, collection } from "firebase/firestore";
 import withAuth from "../../components/withAuth";
 import { LoadingButton } from "@mui/lab";
@@ -24,18 +25,20 @@ const selectOptions = [
 
 // Default wine image
 const photoUrl =
-  "https://www.weaverswines.com/media/catalog/product/placeholder/default/Place_Holder_weavers_1_1.jpg";
+  "https://firebasestorage.googleapis.com/v0/b/fir-test-97ab8.appspot.com/o/wine_placeholder.jpg?alt=media&token=3f4acd10-875f-46f9-b1ab-d2b66cc3f152";
 
 const AddWine = () => {
   const Router = useRouter();
   const { triggerSnackbar } = useContext(SnackbarContext);
+  const { currentUserId } = useContext(AuthContext);
+
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [region, setRegion] = useState("");
   const [selectValue, setSelectValue] = useState("red");
-
-  // add validation, shouldnt be able to submit empty doc
 
   const addWineToFirestore = () => {
     // Get collection
@@ -43,29 +46,39 @@ const AddWine = () => {
     const db = getFirestore();
     const winesColRef = collection(db, "wines");
 
+    // Loading Button state and validation
     setLoading(true);
+    setError(false);
 
-    addDoc(winesColRef, {
-      name,
-      price,
-      region,
-      rating: [],
-      type: selectValue,
-      photoUrl,
-    })
-      .then(() => {
-        setLoading(false);
-        Router.push("/");
-        triggerSnackbar(
-          "success",
-          "Your wine has been successfuly added to collection."
-        );
+    // Check for empty inputs
+    if (name === "" || price === "" || region === "") {
+      setError(true);
+      triggerSnackbar("error", "All fields are required.");
+      setLoading(false);
+    } else {
+      addDoc(winesColRef, {
+        name,
+        price,
+        region,
+        rating: [],
+        type: selectValue,
+        photoUrl,
+        addedBy: currentUserId,
       })
-      .catch((e) => {
-        setLoading(false);
-        console.warn(e);
-        triggerSnackbar(e.code.split(5));
-      });
+        .then(() => {
+          setLoading(false);
+          Router.push("/");
+          triggerSnackbar(
+            "success",
+            "Your wine has been successfuly added to collection."
+          );
+        })
+        .catch((e) => {
+          setLoading(false);
+          console.warn(e);
+          triggerSnackbar(e.code.split(5));
+        });
+    }
   };
 
   return (
@@ -97,6 +110,7 @@ const AddWine = () => {
           value={name}
           onChange={(e) => setName(e.target.value)}
           margin="normal"
+          error={error}
         />
         <TextField
           fullWidth
@@ -106,6 +120,7 @@ const AddWine = () => {
           value={price}
           onChange={(e) => setPrice(e.target.value)}
           margin="normal"
+          error={error}
         />
         <TextField
           fullWidth
@@ -114,6 +129,7 @@ const AddWine = () => {
           value={region}
           onChange={(e) => setRegion(e.target.value)}
           margin="normal"
+          error={error}
         />
         <TextField
           fullWidth
